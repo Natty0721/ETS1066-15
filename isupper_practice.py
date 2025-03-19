6eadc52bc80ab047195ccdd11cb953ebebe0f8fc
ETS1066/15 <natnaelayalew21@gmail.com>
2025-03-19 11:34:13 -0900

    First Practice

---
+++ isupper_practice.py
@@ -0,0 +1,21 @@
+# For my first day I choose isupper ()
+
+# Practice 1
+
+text1 = "HELLO"
+text2 = "Hello"
+text3 = "123!"
+text4 = "PYTHON123"
+
+print(text1.isupper())  # True (All uppercase)
+print(text2.isupper())  # False (Contains lowercase letters)
+print(text3.isupper())  # False (No uppercase letters)
+print(text4.isupper())  # True (Numbers don't affect the result)
+
+# Practice 2
+
+text = "PYTHON"
+print(text.isupper())  # Output: True
+
+text = "Python"
+print(text.isupper())  # Output: False
