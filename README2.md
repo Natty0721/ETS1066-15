ac490a02cbdf9f623e7882c0adf79f23b552a984
ETS1066/15 <natnaelayalew21@gmail.com>
2025-03-20 23:14:43 -0900

    ReadMe2

---
+++ README2.md
@@ -0,0 +1,26 @@
+# Pythons String Methods: lower(), upper(), and title()
+
+## str.lower()
+ Description
+The `lower()` method converts all alphabetic characters in a string to lowercase.  
+It **does not** change numbers or special characters.
+
+### Syntax
+```python
+string.lower()
+
+## str.upper()
+ Description
+The upper() method converts all alphabetic characters in a string to uppercase.
+
+### 🖥️ Syntax
+python
+string.upper()
+
+### str.title()
+ Description
+The title() method capitalizes the first letter of each word in a string.
+
+### Syntax
+python
+string.title()
---
+++ String_second.py
@@ -0,0 +1,38 @@
+# lower() String
+text0 = "NATNAEL AYALEW"
+print(text0.lower())  # Output: natnael ayalew
+
+text1 = "PYTHON PROGRAMMING"
+print(text1.lower())  # Output: python programming
+
+text2 = "Hello ETHIOPIA 1234"
+print(text2.lower())  # Output: hello ethiopia 1234
+
+text3 = "MeChaTronics MiXeD CaSe"
+print(text3.lower())  # Output: mechatronics mixed case
+
+# upper() String
+text0 = "natnael ayalew"
+print(text0.upper())  # Output: NATNAEL AYALEW
+
+text1 = "python programming"
+print(text1.upper())  # Output: PYTHON PROGRAMMING
+
+text2 = "hello ethiopia 1234"
+print(text2.upper())  # Output: HELLO ETHIOPIA 1234
+
+text3 = "MeChaTronics MiXeD CaSe"
+print(text3.upper())  # Output: MECHATRONICS MIXED CASE
+
+# title() String
+text0 = "natnael ayalew"
+print(text0.title())  # Output: Natnael Ayalew
+
+text1 = "python programming"
+print(text1.title())  # Output: Python Programming
+
+text2 = "hello ethiopia 1234"
+print(text2.title())  # Output: Hello Ethiopia 1234
+
+text3 = "MeChaTronics MiXeD CaSe"
+print(text3.title())  # Output: Mechatronics Mixed Case
