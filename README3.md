c3b79c0b93729362e5a5d256613f8c99a23a756d
ETS1066/15 <natnaelayalew21@gmail.com>
2025-03-23 08:49:05 -0900

    New practice

---
+++ README3.md
@@ -0,0 +1,23 @@
+# Python String Methods: capitalize(), swapcase(), and find()
+
+1. str.capitalize()
+Description
+The `capitalize()` method converts the first letter of a string to uppercase, and the rest to lowercase.
+
+# Syntax at python
+string.capitalize()
+
+2. str.swapcase()
+📌 Description
+The swapcase() method switches the case of all letters.
+
+# Syntax at python
+string.swapcase()
+
+3. str.find()
+# Description
+The find() method searches for a substring and returns the index of its first occurrence.
+If not found, it returns -1.
+
+# Syntax at python
+string.find(substring)
---
+++ String_new.py
@@ -0,0 +1,29 @@
+# capitalize() String
+text1 = "natnael ayalew"
+print(text1.capitalize())  # Output: Natnael ayalew
+
+text2 = "pYTHON proGRAMming"
+print(text2.capitalize())  # Output: Python programming
+
+text3 = "python 1234"
+print(text3.capitalize())  # Output: Python 1234
+
+# swapcase() String
+text1 = "Natnael Ayalew"
+print(text1.swapcase())  # Output: nATNAEL aYALEW
+
+text2 = "PyThOn ProGraMMing"
+print(text2.swapcase())  # Output: pYtHoN pROgRAmMING
+
+text3 = "Python 1234"
+print(text3.swapcase())  # Output: pYTHON 1234
+
+# find() String
+text1 = "Natnael Ayalew is a student"
+print(text1.find("Ayalew"))  # Output: 8 (Index where "Ayalew" starts)
+
+text2 = "Python programming is fun"
+print(text2.find("gram"))  # Output: 10
+
+text3 = "Python 1234"
+print(text3.find("robot"))  # Output: -1 (Substring not found)
